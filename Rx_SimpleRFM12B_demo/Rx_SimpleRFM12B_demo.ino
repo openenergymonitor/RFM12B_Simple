1bb4ffbdb021096932d96898c80dbd9f54d11331
Glyn Hudson <glyn.hudson@gmail.com>
2012-04-12 01:58:45 +0100

    update to be consistant with emonTx wireless packet structure

---
+++ Rx_SimpleRFM12B_demo/Rx_SimpleRFM12B_demo.ino
@@ -0,0 +1,48 @@
+//Simple RFM12B wireless demo - Receiver - no ack
+//Glyn Hudson openenergymonitor.org GNU GPL V3 12/4/12
+//Credit to JCW from Jeelabs.org for RFM12 
+
+#include <JeeLib.h>
+
+#define myNodeID 30          //node ID of Rx (range 0-30) 
+#define network     210      //network group (can be in the range 1-250).
+#define freq RF12_433MHZ     //Freq of RF12B can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. Match freq to module
+
+typedef struct { int power1, power2, power3, battery; } PayloadTX;      // create structure - a neat way of packaging data for RF comms
+PayloadTX emontx;  
+
+const int emonTx_NodeID=10;            //emonTx node ID
+
+void setup() {
+  
+  rf12_initialize(myNodeID,freq,network);   //Initialize RFM12 with settings defined above  
+  Serial.begin(9600); 
+  Serial.println("RF12B demo Receiver - Simple demo"); 
+  
+ Serial.print("Node: "); 
+ Serial.print(myNodeID); 
+ Serial.print(" Freq: "); 
+ if (freq == RF12_433MHZ) Serial.print("433Mhz");
+ if (freq == RF12_868MHZ) Serial.print("868Mhz");
+ if (freq == RF12_915MHZ) Serial.print("915Mhz"); 
+ Serial.print(" Network: "); 
+ Serial.println(network);
+}
+
+void loop() {
+  
+ if (rf12_recvDone()){    
+  if (rf12_crc == 0 && (rf12_hdr & RF12_HDR_CTL) == 0) {
+    
+    int node_id = (rf12_hdr & 0x1F);		  //extract nodeID from payload
+        
+    if (node_id == emonTx_NodeID)  {             //check data is coming from node with the corrct ID
+        emontx=*(PayloadTX*) rf12_data;            // Extract the data from the payload 
+       Serial.print("power1: "); Serial.println(emontx.power1); 
+       Serial.print("power2: "); Serial.println(emontx.power2); 
+       Serial.print("power3: "); Serial.println(emontx.power3); 
+       Serial.print("battery: "); Serial.println(emontx.battery); 
+       Serial.println("  "); 
+  }
+ }
+}
---
+++ Tx_SimpleRFM12B_demo/Tx_SimpleRFM12B_demo.ino
@@ -0,0 +1,48 @@
+//Simple RFM12B wireless demo - transimtter - no ack
+//Glyn Hudson openenergymonitor.org GNU GPL V3 7/7/11
+//Credit to JCW from Jeelabs.org for RFM12 
+
+#include <RF12.h>
+#include <Ports.h> //from jeelabs.org
+
+#define myNodeID 10          //node ID of tx (range 0-30)
+#define network     210      //network group (can be in the range 1-250).
+#define freq RF12_433MHZ     //Freq of RF12B can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. Match freq to module
+
+
+typedef struct { int power1, power2, power3, battery; } PayloadTX;      // create structure - a neat way of packaging data for RF comms
+PayloadTX emontx;  
+
+void setup() {
+  rf12_initialize(myNodeID,freq,network);   //Initialize RFM12 with settings defined above  
+Serial.begin(9600);
+Serial.println("RFM12B Transmitter - Simple demo");
+
+ Serial.print("Node: "); 
+ Serial.print(myNodeID); 
+ Serial.print(" Freq: "); 
+ if (freq == RF12_433MHZ) Serial.print("433Mhz");
+ if (freq == RF12_868MHZ) Serial.print("868Mhz");
+ if (freq == RF12_915MHZ) Serial.print("915Mhz"); 
+ Serial.print(" Network: "); 
+ Serial.println(network);
+
+}
+
+void loop() {
+  emontx.power1=emontx.power1+1;
+  emontx.power2=emontx.power2+2;
+  emontx.power3=emontx.power3+3;
+  emontx.battery=emontx.battery+4;
+ 
+  int i = 0; while (!rf12_canSend() && i<10) {rf12_recvDone(); i++;}
+    rf12_sendStart(0, &emontx, sizeof emontx);
+    
+  Serial.print("power1: "); Serial.println(emontx.power1); 
+  Serial.print("power2: "); Serial.println(emontx.power2); 
+  Serial.print("power3: "); Serial.println(emontx.power3); 
+  Serial.print("battery: "); Serial.println(emontx.battery); 
+  Serial.println("  "); 
+         
+  delay(2000);
+}
